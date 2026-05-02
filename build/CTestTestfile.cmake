1451c181a1e2684878ddd735a35c23e7cb76f36d
sashurman <top.tank.tanki.online@gmail.com>
2026-05-02 17:50:31 +0300

    Remove build and binaries from git

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sashurman/search_engine
-# Build directory: /home/sashurman/search_engine/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
